9fe99dc0785ffdc834a8d257da4b9314158005ae
Christian Lück <christian@lueck.tv>
2016-03-04 00:51:04 +0100

    More in line with how TcpConnector/StreamEncryption class works

--- f4.php
+++ f4.php
@@ -3,16 +3,23 @@
 $f = stream_socket_client('tcp://self-signed.badssl.com:443', $errstr, $errno, 0, STREAM_CLIENT_CONNECT|STREAM_CLIENT_ASYNC_CONNECT);
 stream_set_blocking($f, 0);
 
+// wait for connection to succeed
 $n = null; $w = array($f);
-stream_select($n, $w, $n, 10);
+stream_select($n, $w, $n, null);
 
 stream_context_set_option($f, 'ssl', 'verify_peer', true);
 
 while (true) {
-    $r = stream_socket_enable_crypto($f, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
-    if ($r !== 0) {
+    // try to enable crypto
+    $o = stream_socket_enable_crypto($f, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+
+    // 0=keep trying, otherwise success/error
+    if ($o !== 0) {
         break;
     }
-    usleep(100000);
+
+    // try again once we receive data
+    $r = array($f);
+    stream_select($r, $n, $n, null);
 }
 
