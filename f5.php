d4aecb6543b89027b3c54989f2e72f62a2a9f419
Christian Lück <christian@lueck.tv>
2016-03-04 01:03:38 +0100

    FU only IP, add full context

---
+++ f5.php
@@ -0,0 +1,28 @@
+<?php
+
+$ip = gethostbyname('self-signed.badssl.com');
+$f = stream_socket_client('tcp://' . $ip . ':443', $errstr, $errno, 0, STREAM_CLIENT_CONNECT|STREAM_CLIENT_ASYNC_CONNECT);
+stream_set_blocking($f, 0);
+
+// wait for connection to succeed
+$n = null; $w = array($f);
+stream_select($n, $w, $n, null);
+
+stream_context_set_option($f, 'ssl', 'SNI_enabled', true);
+stream_context_set_option($f, 'ssl', 'SNI_server_name', true);
+stream_context_set_option($f, 'ssl', 'verify_peer', true);
+
+while (true) {
+    // try to enable crypto
+    $o = stream_socket_enable_crypto($f, true, STREAM_CRYPTO_METHOD_TLS_CLIENT);
+
+    // 0=keep trying, otherwise success/error
+    if ($o !== 0) {
+        break;
+    }
+
+    // try again once we receive data
+    $r = array($f);
+    stream_select($r, $n, $n, null);
+}
+
